e67a888aee547ac25013306f8e3f6dca037719bb
Anastasiya Semenkevich <anastasija.ig@gmail.com>
2023-10-23 15:34:46 +0300

    feature: text input cleared

--- todo_app_web.py
+++ todo_app_web.py
@@ -5,17 +5,31 @@ import streamlit as st
 
 todos_list = functions.get_todos_from_file()
 
+st.set_page_config(
+    layout="centered",
+    menu_items={
+        'About': "This is a cool app :)"
+    })
+
 
 def add_todo_to_the_list():
     todo = st.session_state["new_todo_input"] + '\n'
-    todos_list.append(todo)
+    if todo != '\n':
+        todos_list.append(todo)
     functions.write_todos_to_file(todos_list)
+    st.session_state['new_todo_input'] = ""
 
 
 st.title("My Todo App")
-st.subheader("This is my todo app")
-st.write("This app is to increase your productivity" + '\n')
-st.write("Today is " + time.strftime("%b %d, %Y %H:%M:%S") + " Crush it!")
+st.subheader("Plan. Complete. Simple.")
+st.write("This app is to increase <b>your productivity</b>", unsafe_allow_html=True)
+st.write("Today is " + time.strftime("%b %d, %Y"))
+
+st.text_input(
+    label="Enter a todo here",
+    placeholder="What is your next todo?",
+    on_change=add_todo_to_the_list,
+    key='new_todo_input')
 
 for index, item in enumerate(todos_list):
 
@@ -25,10 +39,6 @@ for index, item in enumerate(todos_list):
         todos_list.pop(index)
         functions.write_todos_to_file(todos_list)
         del st.session_state[f"checkbox_{index}"]
-        st.experimental_rerun()
+        st.rerun()
 
-st.text_input(
-    label="Enter a todo here",
-    placeholder="Add a new todo...",
-    on_change=add_todo_to_the_list,
-    key='new_todo_input')
+st.session_state
